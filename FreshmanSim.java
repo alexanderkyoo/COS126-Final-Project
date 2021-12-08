831e1a016bbd2e732b504814947598c2a6c12763
david-oke <58450661+david-oke@users.noreply.github.com>
2021-12-08 13:58:59 -0500

    Update FreshmanSim.java

--- FreshmanSim.java
+++ FreshmanSim.java
@@ -1,7 +1,70 @@
 public class FreshmanSim {
+
     public static void main(String[] args) {
-        
+
         StdOut.println("Welcome to the Princeton Freshman Simulator!");
+        StdOut.println("Please select your difficulty: ");
+        StdOut.println("------------------------------");
+        StdOut.println("(1) - Easy" + "\n" + "(2) - Medium" + "\n" + "(3) - Hard");
+        int difficulty = StdIn.readInt();
+        Person player;
+        double balance = 0.0;
+        Financials bank;
+        boolean levelChecked = false;
+
+        while (!levelChecked) {
+            if (difficulty == 1) {
+                StdOut.println("Easy Mode...");
+                levelChecked = true;
+                balance = 5000;
+                bank = new Financials(balance);
+                player = new Person(100, 100, bank);
+                StdOut.println(player);
+            }
+            else if (difficulty == 2) {
+                StdOut.println("Medium Mode...");
+                levelChecked = true;
+                balance = 2500;
+                bank = new Financials(balance);
+                player = new Person(80, 80, bank);
+                StdOut.println(player);
+            }
+            else if (difficulty == 3) {
+                StdOut.println("Hard Mode...");
+                levelChecked = true;
+                balance = 100;
+                bank = new Financials(balance);
+                player = new Person(50, 50, bank);
+                StdOut.println(player);
+            }
+            else {
+                StdOut.println("Please select an applicable difficulty level.");
+                difficulty = StdIn.readInt();
+            }
+        }
+
+        StdOut.println("Select your general concentration area:");
+        StdOut.println("(1) - B.S.E." + "\n" + "(2) - A.B." + "\n");
+        int concentration = StdIn.readInt();
+        boolean concentrationChecked = false;
+
+        while (!concentrationChecked) {
+            if (concentration == 1) {
+                StdOut.println("You selected B.S.E.");
+                Course course = new Course("BSE");
+                concentrationChecked = true;
+            }
+            else if (concentration == 2) {
+                StdOut.println("You selected A.B.");
+                Course course = new Course("AB");
+                concentrationChecked = true;
+            }
+            else {
+                StdOut.println("Please select an applicable concentration area.");
+                concentration = StdIn.readInt();
+            }
+        }
+
 
     }
 }
