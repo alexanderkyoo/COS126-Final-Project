ea9cea481276fe481e1376d0eb8b49d323bb0444
algxa <95768413+algxa@users.noreply.github.com>
2021-12-08 13:04:52 -0500

    Update Course.java
    yes

--- Course.java
+++ Course.java
@@ -1,4 +1,33 @@
+import java.util.ArrayList;
+
 public class Course {
+    private String Cname;
+    private ArrayList<Double> grades;
+
+    public Course(String name) {
+        Cname = name;
+    }
+
+    public void addGrade(double grade) {
+        grades.add(grade);
+    }
+
+    public String getName()
+    {
+        return Cname;
+    }
+    
+    public double getAvg() {
+        if (grades.isEmpty()) {
+            return 100;
+        }
+
+        int avg = 0;
+        for (int i = 0; i < grades.size(); i++) {
+            avg += grades.get(i);
+        }
+        return avg / (grades.size());
+    }
 
     public static void main(String[] args) {
 
