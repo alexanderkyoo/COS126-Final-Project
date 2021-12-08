9a6b0f9d24c3ec39bc37ea867bf5b1cc82028fdf
david-oke <58450661+david-oke@users.noreply.github.com>
2021-12-08 13:59:19 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -2,5 +2,20 @@ public class Person {
     private int health;
     private int happiness;
     private Financials bank; // Represents person's bank account
-    private double money; // Money being carried around
+
+    public Person(int health, int happiness, Financials bank) {
+        this.health = health;
+        this.happiness = happiness;
+        this.bank = bank;
+    }
+
+
+    public String toString() {
+        return "<<<<<<<<<<<<<<<<<<< Player Attributes >>>>>>>>>>>>>>>>>>>" + "\n"
+                + "Health : " + health + "\n"
+                + "Happiness: " + happiness + "\n"
+                + "Bank balance: " + bank.getBalance() + "\n"
+                + "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
+    }
+
 }
