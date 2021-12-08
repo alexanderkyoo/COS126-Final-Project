222735db7acda27bb9ea5a289928388d77b9052d
algxa <95768413+algxa@users.noreply.github.com>
2021-12-08 13:44:00 -0500

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,5 +1,3 @@
-
-
 import java.util.ArrayList;
 
 public class Course {
@@ -14,8 +12,7 @@ public class Course {
         grades.add(grade);
     }
 
-    public String getName()
-    {
+    public String getName() {
         return cName;
     }
 
@@ -29,6 +26,22 @@ public class Course {
             avg += grades.get(i);
         }
         return avg / (grades.size());
+    }
+
+    public double assignment(int size, double time, int intelligence) {
+        double score = 0;
+        if (time == 0)
+        {
+            return 0;
+        }
+        else if (5 * size * time >= intelligence)
+        {
+            return intelligence;
+        }
+        else
+        {
+            return StdRandom.uniform(5 * size * time, intelligence);
+        }
     }
 
     public static void main(String[] args) {
