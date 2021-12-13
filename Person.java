a37bf3971a67e59b874b97d8b6aeaf403e04cdb0
david-oke <58450661+david-oke@users.noreply.github.com>
2021-12-13 13:10:44 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -2,11 +2,17 @@ public class Person {
     private int health;
     private int happiness;
     private Financials bank; // Represents person's bank account
+    private int intelligence;
 
-    public Person(int health, int happiness, Financials bank) {
+    public Person(int health, int happiness, Financials bank, int intelligence) {
         this.health = health;
         this.happiness = happiness;
         this.bank = bank;
+        this.intelligence = intelligence;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
     }
 
 
@@ -15,6 +21,7 @@ public class Person {
                 + "Health : " + health + "\n"
                 + "Happiness: " + happiness + "\n"
                 + "Bank balance: " + bank.getBalance() + "\n"
+                + "Intelligence: " + intelligence + "\n"
                 + "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
     }
 
