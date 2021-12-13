38e253e3f03be38b41d3df50666280307c44375d
algxa <95768413+algxa@users.noreply.github.com>
2021-12-12 22:44:48 -0500

    hi david

--- Assignment.java
+++ Assignment.java
@@ -1,26 +1,23 @@
 public class Assignment {
-    private String name;
     private int tier;
-    private String topic;
-
-    public String getName() {
-        return name;
-    }
 
     public int getTier() {
         return tier;
     }
-
-    public String getTopic() {
-        return topic;
-    }
-
+    
+    
     public Assignment(String name, int tier, String topic) {
-        this.name = name;
         this.tier = tier;
-        this.topic = topic;
     }
 
+    public static String generateAssignment(Course horse){
+        if (horse.getName().equals("AB"))
+        {
+            
+        }
+        
+        
+    }
     public static void main(String[] args) {
         StdOut.println("Hi I'm useless");
     }
