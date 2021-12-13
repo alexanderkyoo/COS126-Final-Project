ff10e91a480e421b0849e4d73267366ac68fc234
david-oke <58450661+david-oke@users.noreply.github.com>
2021-12-13 13:08:04 -0500

    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -1,24 +1,67 @@
 public class Assignment {
-    private int tier;
+    private static String assignmentType;
+    private static String assignmentTopic;
+    private static String assignmentMessage;
+    private static int tier;
+    private double score;
+
+    public Assignment(String type, int assignmentTier, String topic, String message) {
+        tier = assignmentTier;
+        assignmentTopic = topic;
+        assignmentType = type;
+        assignmentMessage = message;
+        score = 0.0;
+    }
 
     public int getTier() {
         return tier;
     }
-    
-    
-    public Assignment(String name, int tier, String topic) {
-        this.tier = tier;
+
+    public void setScore(double score) {
+        this.score = score;
     }
 
-    public static String generateAssignment(Course horse){
-        if (horse.getName().equals("AB"))
-        {
-            
+    public double getScore() {
+        return score;
+    }
+
+    public static Assignment generateAssignment(Course course) {
+        String[] topicArrayAB = {
+                "18th century sex appeal", "gargoyles", "the history of P = NP",
+                "monarchs", "the emu war"
+        };
+        String[] assignmentTypeAB = {
+                "paper", "reading", "discussion", "close reading", "exam"
+        };
+
+        String[] topicArrayBSE = {
+                "multivariable calculus", "titrations", "geometry of plates",
+                "intermolecular bonding", "the reproductive system", "rat anatomy",
+                "inheritance", "recursion", "Sierpinski's triangles", "boolean algebra"
+        };
+        String[] assignmentTypeBSE = {
+                "problem set", "exam", "exam prep", "lab report", "quiz"
+        };
+
+        if (course.getName().equals("BSE")) {
+            int assignmentTypeIndex = StdRandom.uniform(assignmentTypeBSE.length);
+            assignmentType = assignmentTypeBSE[assignmentTypeIndex];
+            int topicIndex = StdRandom.uniform(topicArrayBSE.length);
+            assignmentTopic = topicArrayBSE[topicIndex];
         }
-        
-        
+        else if (course.getName().equals("AB")) {
+            int assignmentTypeIndex = StdRandom.uniform(assignmentTypeAB.length);
+            assignmentType = assignmentTypeAB[assignmentTypeIndex];
+            int topicIndex = StdRandom.uniform(topicArrayAB.length);
+            assignmentTopic = topicArrayAB[topicIndex];
+        }
+        String message = "You have been assigned a(n) " + assignmentType
+                + " on " + assignmentTopic;
+        return new Assignment(assignmentType, tier, assignmentTopic, message);
     }
-    public static void main(String[] args) {
-        StdOut.println("Hi I'm useless");
+
+    public String toString() {
+        return assignmentMessage;
     }
+    
 }
