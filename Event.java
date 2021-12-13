c0b09c61636459634e986fba8df4de55df2c8ed0
algxa <95768413+algxa@users.noreply.github.com>
2021-12-13 09:43:06 -0500

    Update Event.java

--- Event.java
+++ Event.java
@@ -9,20 +9,46 @@ public class Event {
         this.voluntary = voluntary;
     }
 
-    public static String generateEvent(int charisma) {
+    public String getDescrip() {
+        return descrip;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public boolean isVoluntary() {
+        return voluntary;
+    }
+
+    public static Event generateEvent(int charisma) {
+        Event nextEvent;
+        String name;
+        double price;
+        boolean willing;
         if (charisma > 80) {
             String[] events = {
                     "presidential nomination", "secret party", "VIP get-together",
                     "ritualistic headbutting ceremony", "pickup game"
             };
+            name = events[StdRandom.uniform(events.length)];
+            price = StdRandom.uniform(0, 40) * 10;
+            willing = true;
         }
         else if (charisma > 40) {
             String[] events = { "party", "eat out", "grill", "street fight", "hit job" };
+            name = events[StdRandom.uniform(events.length)];
+            price = StdRandom.uniform(0, 20) * 10;
+            willing = true;
         }
         else {
             String[] events = { "mugging", "wedgie", "blackmail", "coercion", "extortion" };
+            name = events[StdRandom.uniform(events.length)];
+            price = Double.MAX_VALUE;
+            willing = false;
         }
-        return "You participated in a(n) " + events[StdRandom.uniform(events.length)];
+        nextEvent = new Event(name, price, willing);
+        return nextEvent;
     }
 
     public static void main(String[] args) {
