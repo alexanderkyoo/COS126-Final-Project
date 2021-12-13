752d92a26b94fe7a45e1e2136ede1ef9ffd55908
algxa <95768413+algxa@users.noreply.github.com>
2021-12-13 18:02:24 -0500

    advanceday progresss

--- FreshmanSim.java
+++ FreshmanSim.java
@@ -1,9 +1,31 @@
 public class FreshmanSim {
+    private static int day = 0;
+    private static Person player;
+    private static Course course;
+    private static Grades grades;
 
     public static void advanceDay() {
         // run assignment and event generators
         // ask user how many hours to spend on each assignment/event
         // calculate corresponding grade changes, attribute changes, etc.
+        StdOut.println("Day " + day + " begins!");
+        int hours = 24;
+        Assignment todays = Assignment.generateAssignment(course);
+        StdOut.println("How long do you wish to spend on this " + todays.getAssignmentType() + "?");
+        StdOut.println("------------------------------");
+        StdOut.println("Enter a number from 0 to " + hours + ":");
+        int work = StdIn.readInt();
+        boolean hoursChecked = false;
+        while (!hoursChecked) {
+            if (work >= 0 || work <= hours) {
+                hours -= work;
+                grades.addAssignment(todays);
+                grades.scoreAssignment(todays, player);
+                StdOut.println(
+                        "You scored a " + todays.getScore() + todays.getAssignmentType() + ".");
+            }
+        }
+
     }
 
     public static void main(String[] args) {
@@ -13,7 +35,6 @@ public class FreshmanSim {
         StdOut.println("------------------------------");
         StdOut.println("(1) - Easy" + "\n" + "(2) - Medium" + "\n" + "(3) - Hard");
         int difficulty = StdIn.readInt();
-        Person player;
         double balance = 0.0;
         Financials bank;
         boolean levelChecked = false;
@@ -57,12 +78,12 @@ public class FreshmanSim {
         while (!concentrationChecked) {
             if (concentration == 1) {
                 StdOut.println("You selected B.S.E.");
-                Course course = new Course("BSE");
+                course = new Course("BSE");
                 concentrationChecked = true;
             }
             else if (concentration == 2) {
                 StdOut.println("You selected A.B.");
-                Course course = new Course("AB");
+                course = new Course("AB");
                 concentrationChecked = true;
             }
             else {
