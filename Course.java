cb5c37c844a7eec4009cf3a6b5c77187b08e18fa
algxa <95768413+algxa@users.noreply.github.com>
2021-12-14 02:21:01 -0500

    Bezos plays for the milwaukee bucks

--- Course.java
+++ Course.java
@@ -1,11 +1,19 @@
+// Represents a Course that has a name
 public class Course {
-    private String cName;
+    private String cName; // Represents the name of the course
 
     public Course(String name) {
         cName = name;
     }
 
-    public String getName() {
+    // Retrieves the course name and also functions as the toString method
+    public String toString() {
         return cName;
+    }
+
+
+    public static void main(String[] args) {
+        Course bezosBuks = new Course("Bezos Bucks");
+        StdOut.println(bezosBuks);
     }
 }
