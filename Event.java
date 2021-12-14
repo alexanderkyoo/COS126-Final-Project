c35460a70f02ccc96438285d8ef70c2a75c2188e
david-oke <58450661+david-oke@users.noreply.github.com>
2021-12-14 03:05:41 -0500

    Update Event.java

--- Event.java
+++ Event.java
@@ -3,6 +3,16 @@ public class Event {
     private String descrip;
     private double cost;
     private boolean voluntary;
+    private int involuntaryHours = 0;
+
+    public Event(String descrip, double cost, boolean voluntary, String message, int hours) {
+        this.descrip = descrip;
+        this.cost = cost;
+        this.voluntary = voluntary;
+        eventMessage = message;
+        if (!voluntary)
+            involuntaryHours = hours;
+    }
 
     public Event(String descrip, double cost, boolean voluntary, String message) {
         this.descrip = descrip;
@@ -23,6 +33,14 @@ public class Event {
         return voluntary;
     }
 
+    public String getEventMessage() {
+        return eventMessage;
+    }
+
+    public int getHours() {
+        return involuntaryHours;
+    }
+
     public static Event generateEvent(int charisma) {
         String name, message;
         double price;
@@ -33,27 +51,31 @@ public class Event {
                     "ritualistic headbutting ceremony", "pickup game"
             };
             name = events[StdRandom.uniform(events.length)];
-            price = StdRandom.uniform(0, 40) * 10;
+            price = StdRandom.uniform(0, 400);
             willing = true;
         }
-        else if (charisma > 40) {
+        else if (charisma > 30) {
             String[] events = { "party", "eat out", "grill", "street fight", "hit job" };
             name = events[StdRandom.uniform(events.length)];
-            price = StdRandom.uniform(0, 20) * 10;
+            price = StdRandom.uniform(0, 200);
             willing = true;
         }
         else {
             String[] events = { "mugging", "wedgie", "blackmail", "coercion", "extortion" };
             name = events[StdRandom.uniform(events.length)];
-            price = StdRandom.uniform(35, 55) * 10;
+            price = StdRandom.uniform(400, 550);
             willing = false;
         }
-        if (willing)
+        if (willing) {
             message = "You have been invited to a " + name;
-        else
-            message = "You are involved in a(n) " + name;
-
-        return new Event(name, price, willing, message);
+            return new Event(name, price, true, message);
+        }
+        else {
+            int hours = StdRandom.uniform(1, 6);
+            message = "You are involved in a(n) " + name + ". This will take " + hours
+                    + " hours.";
+            return new Event(name, price, false, message, hours);
+        }
     }
 
     public String toString() {
