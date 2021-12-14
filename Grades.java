df4307ef2ef3c1c2c2ccd435964c3ff575bf1b58
algxa <95768413+algxa@users.noreply.github.com>
2021-12-13 22:44:43 -0500

    oob

--- Grades.java
+++ Grades.java
@@ -7,29 +7,28 @@ public class Grades {
         assignmentList.add(a);
     }
 
-    public static void scoreAssignment(Assignment a, Person p) {
+    public static void scoreAssignment(Assignment a, Person p, int hours) {
         int tier = a.getTier();
         int intelligence = p.getIntelligence();
-        // SAMPLE CALCULATION --> change this calculation later
-        double score = tier * intelligence;
+        double score = ((double) tier) / 5 * hours * hours + intelligence;
         a.setScore(score);
     }
 
     public static double calculateGPA() {
         if (assignmentList.size() == 0)
             return 100.0;
-
+        double weight = 0.0;
         double total = 0.0;
         for (Assignment a : assignmentList) {
-            total += a.getScore();
+            weight += 1 / (double) a.getTier();
+            total += (1 / (double) a.getTier()) * a.getScore();
         }
-        return total / assignmentList.size();
+        return total / weight;
     }
 
     /* public String assignment(String class, int time, int intelligence) {
         int index = 0;
         for (int i = 0; i < classes.length; i++) {
-
         }
     } */
 }
