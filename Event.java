262962725dc0774e0a4f73e3bc0a01e0be2e7652
algxa <95768413+algxa@users.noreply.github.com>
2021-12-14 03:41:45 -0500

    yelp

--- Event.java
+++ Event.java
@@ -1,9 +1,11 @@
+// Represents an event that could happen to the player
+// Allows for the generation of an event, within a set list
 public class Event {
-    private String eventMessage;
-    private String descrip;
-    private double cost;
-    private boolean voluntary;
-    private int involuntaryHours = 0;
+    private String eventMessage; // Represents the String form of the event
+    private String descrip; // Represents a description of the event
+    private double cost; // Represents the cost of the event
+    private boolean voluntary; // Is the event voluntary
+    private int involuntaryHours = 0; // Represents length of an involuntary event
 
     public Event(String descrip, double cost, boolean voluntary, String message, int hours) {
         this.descrip = descrip;
@@ -21,26 +23,32 @@ public class Event {
         eventMessage = message;
     }
 
+    // Retrieves the description of the event
     public String getDescrip() {
         return descrip;
     }
 
+    // Retrieves the cost of the event
     public double getCost() {
         return cost;
     }
 
+    // Retrieves whether the event is voluntary
     public boolean isVoluntary() {
         return voluntary;
     }
 
+    // Retrieves the event message
     public String getEventMessage() {
         return eventMessage;
     }
 
+    // Retrieves the number of hours spent in the involuntary event
     public int getHours() {
         return involuntaryHours;
     }
 
+    // Generates an event, based on charisma, and based on a preset list of events
     public static Event generateEvent(int charisma) {
         String name, message;
         double price;
@@ -78,7 +86,24 @@ public class Event {
         }
     }
 
+    // Converts the event into a String format
     public String toString() {
         return eventMessage;
     }
+
+    public static void main(String[] args) {
+        Event ex1 = Event.generateEvent(100);
+        StdOut.println(ex1.getDescrip());
+        StdOut.println(ex1.getCost());
+        StdOut.println(ex1);
+        Event ex2 = Event.generateEvent(70);
+        StdOut.println(ex2.isVoluntary());
+        StdOut.println(ex2.getEventMessage());
+        StdOut.println(ex2);
+        Event ex3 = Event.generateEvent(10);
+        StdOut.println(ex3.isVoluntary());
+        StdOut.println(ex3.involuntaryHours);
+        StdOut.println(ex3);
+    }
 }
+
