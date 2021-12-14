d4b1979de894c5e12e59648bb1fbd8ae551c789a
algxa <95768413+algxa@users.noreply.github.com>
2021-12-14 02:08:20 -0500

    huh

--- Person.java
+++ Person.java
@@ -1,8 +1,10 @@
+// Represents the main person in the Freshman Simulator
+// Allows for alteration of basic stats
 public class Person {
-    private int health;
-    private int happiness;
+    private int health; // Numerically represents player's overall health
+    private int happiness; // Numerically represents player's overall happiness
     private Financials bank; // Represents person's bank account
-    private int intelligence;
+    private int intelligence; // Numerically represents person's intelligence
 
     public Person(int health, int happiness, Financials bank, int intelligence) {
         this.health = health;
@@ -11,10 +13,14 @@ public class Person {
         this.intelligence = intelligence;
     }
 
+    // Increments player's happiness by a, which can be negative or positive
     public void incrementHappiness(int a) {
         happiness += a;
     }
 
+    // Increments player's health by a, which can be negative or positive
+    // We thought it should be harder to gain health than lose health
+    // So we added the factor of 2 for negative incrementation
     public void incrementHealth(int a) {
         if (a < 0) {
             health += 2 * a;
@@ -24,23 +30,27 @@ public class Person {
         }
     }
 
+    // Increments player's intelligence by a, which can be negative or positive
     public void incrementintelligence(int a) {
         intelligence += a;
     }
 
+    // Retrieves player's health
     public int getHealth() {
         return health;
     }
 
+    // Retrieves player's happiness
     public int getHappiness() {
         return happiness;
     }
 
+    // Retrieves player's intelligence
     public int getIntelligence() {
         return intelligence;
     }
 
-
+    // Converts the person into String format by displaying attributes
     public String toString() {
         return "<<<<<<<<<<<<<<<<<<< Player Attributes >>>>>>>>>>>>>>>>>>>" + "\n"
                 + "Health : " + health + "\n"
@@ -50,4 +60,16 @@ public class Person {
                 + "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
     }
 
+    public static void main(String[] args) {
+        Financials money = new Financials(1986);
+        Person bezos = new Person(1, 2, money, 3);
+        StdOut.println(bezos);
+        bezos.incrementHealth(5);
+        bezos.incrementHappiness(6);
+        bezos.incrementintelligence(2);
+        StdOut.println(bezos.getHealth());
+        StdOut.println(bezos.getHappiness());
+        StdOut.println(bezos.getIntelligence());
+
+    }
 }
